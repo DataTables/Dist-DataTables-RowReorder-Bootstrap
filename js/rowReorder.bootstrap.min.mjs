5377c226b6d59d28a84f95aa80364679b919c016
Allan Jardine <allan@datatables.net>
2022-11-04 14:53:29 +0000

    Missing files in DataTables-DataTables

--- js/rowReorder.bootstrap.min.mjs
+++ js/rowReorder.bootstrap.min.mjs
@@ -1,4 +1,1 @@
-/*! Bootstrap 3 styling wrapper for RowReorder
- * © SpryMedia Ltd - datatables.net/license
- */
 import $ from"jquery";import DataTable from"datatables.net-bs";import DataTable from"datatables.net-rowreorder";export default DataTable;
