8990a7b270488b2a85673c2837991dfca51bc1f5
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:52:23 +0000

    0a5cd3f764f3794bdd00e0ac929a3846aae95a75 Release 1.3.0
    Sync to source repo @0a5cd3f764f3794bdd00e0ac929a3846aae95a75

--- js/rowReorder.bootstrap.mjs
+++ js/rowReorder.bootstrap.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 3 styling wrapper for RowReorder
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs';
-import DataTable from 'datatables.net-rowreorder';
+import 'datatables.net-rowreorder';
 
 
 
