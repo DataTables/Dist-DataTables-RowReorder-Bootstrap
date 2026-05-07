c478c1c913af38561e5b8759604b23afd55b9926
dtbuild <dtbuild@rigil>
2026-05-07 13:55:30 +0000

    c2d5db8c31749b39063422b1239b2102044e57fd
    Sync to source repo @56bc2a435022c81d3d65663a7793e9c341c9d9c9

--- js/rowReorder.bootstrap.mjs
+++ js/rowReorder.bootstrap.mjs
@@ -1,15 +1,11 @@
-/*! Bootstrap 3 styling wrapper for RowReorder
- * © SpryMedia Ltd - datatables.net/license
+/*! RowReorder Bootstrap 3 styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs';
 import RowReorder from 'datatables.net-rowreorder';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
-
 
 
 
 export default DataTable;
+
